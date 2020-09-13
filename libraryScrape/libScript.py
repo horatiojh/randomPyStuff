34b27deb85fae2c3032ecf81ed689506bbc8fa0d
Horatio Ho <horatiohotness@gmail.com>
2020-09-13 15:32:32 +0800

    Test Commit

--- libraryScrape/libScript.py
+++ libraryScrape/libScript.py
@@ -1,4 +1,13 @@
 from bs4 import BeautifulSoup as bs
 
+
+# Library books that i'm interested in 
+# Milkman
+# https://catalogue.nlb.gov.sg/cgi-bin/spydus.exe/XHLD/WPAC/BIBENQ/95112999/246739648?RECDISP=REC
+# https://catalogue.nlb.gov.sg/cgi-bin/spydus.exe/FULL/WPAC/BIBENQ/95112999/246739648,2
+# https://catalogue.nlb.gov.sg/cgi-bin/spydus.exe/ENQ/WPAC/BIBENQ?optionsDrop=Full+Catalogue&ENTRY=milkman&ENTRY_NAME=BS&ENTRY_TYPE=K&SORTS=SQL_REL_BIB&GQ=milkman&ISGLB=0&NRECS=20&QRY=&QRYTEXT=&FORCESORT=1
+
+
+
 x = 5
-print(x)
+print(x) 
