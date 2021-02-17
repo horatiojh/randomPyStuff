39179069ab3f14e64bc3439c8f20cf54cd19f543
Horatio Ho <horatiohotness@gmail.com>
2021-02-17 18:30:11 +0800

    got a working version! it does its job but is quite slow

--- libraryScrape/libScript.py
+++ libraryScrape/libScript.py
@@ -8,206 +8,143 @@ from bs4 import BeautifulSoup as soup
 import re
 from termcolor import colored
 import sys
-import os 
-import selenium 
-from selenium import webdriver
+import os
+import selenium
 import userInfo
+from selenium import webdriver
 import time
 import pandas as pd
 
-# TODO: See if I can parse directly from my account 
-# TODO: error message if book not found
-
-# new approach: we login to an account and try to get the books from the bookmarked page, 
+# new approach: we login to an account and try to get the books from the bookmarked page,
 # which will circumvent the dynamic URL issue
 
-# begin by importing the username and password from a separate file. I do this so I don't have to keep changing this variable when I push my code changes
+# this function takes in the library of choice as an argument,
+# and returns a dictionary of keys and values,
+# where the key is a string consisting the book title,
+# and the value is a list of statuses
 
-driver = webdriver.Chrome()
-driver.get('https://cassamv2.nlb.gov.sg/cas/login')
+def scrapeAllBookInfo():
+    driver = webdriver.Chrome()
+    driver.get('https://cassamv2.nlb.gov.sg/cas/login')
 
-# select and fill the username
-userIdBox = driver.find_element_by_id('username')
-userIdBox.send_keys(userInfo.username)
+    # begin by importing the username and password from a separate file. I do this so I don't have to keep changing this variable when I push my code changes
 
-# select and fill the password
-passwordBox = driver.find_element_by_id('password')
-passwordBox.send_keys(userInfo.password)
+    # select and fill the username
+    userIdBox = driver.find_element_by_id('username')
+    userIdBox.send_keys(userInfo.username)
 
-loginButton = driver.find_element_by_name('submit')
-loginButton.click()
+    # select and fill the password
+    passwordBox = driver.find_element_by_id('password')
+    passwordBox.send_keys(userInfo.password)
 
-# navigate to the bookmarks page
-driver.get('https://www.nlb.gov.sg/mylibrary/Bookmarks')
+    loginButton = driver.find_element_by_name('submit')
+    loginButton.click()
 
-# get the links and filter/clean them 
-output = driver.find_elements_by_xpath('//a[@class="item-title-value nowrap-fade text-muted"]')
-# output = driver.find_elements_by_tag_name('a')
+    # navigate to the bookmarks page
+    driver.get('https://www.nlb.gov.sg/mylibrary/Bookmarks')
 
-links = []
+    # get the links and filter/clean them
+    output = driver.find_elements_by_xpath('//a[@class="item-title-value nowrap-fade text-muted"]')
 
-for item in output:
-    href = item.get_attribute('href')
-    # we need to append the 'SETLVL=1' if not already set
-    if ("SETLVL" not in href):
-            href = re.sub('BRN', 'SETLVL=1&BRN', href)
-    links.append(href)
+    links = []
+    for item in output:
+        href = item.get_attribute('href')
+        # we need to append the 'SETLVL=1' if not already set
+        if ("SETLVL" not in href):
+                href = re.sub('BRN', 'SETLVL=1&BRN', href)
+        links.append(href)
 
-print(links)
+    # print(links)
 
-infoDict = {}
-# for each link, we need to query the page, click on availability, get the loan info and save it to a list of dataframes 
-for link in links:
-    driver.get(link)
-    time.sleep(1)
-    # get the title of the book 
-    title = driver.find_element_by_xpath('//h5[@class="card-title fullTitleh5"]/span').get_attribute('innerHTML')
-    print(title)
-    output = driver.find_element_by_xpath('//a[@role="button"][@data-target="#holdingsDlg"]')
-    driver.execute_script("arguments[0].click();", output)
-    print('waiting for table')
-    time.sleep(3)
-    # tbl = driver.find_element_by_xpath('//table[@class="table table-stacked"]')
-    tbl = driver.find_element_by_xpath('//table').get_attribute('outerHTML')
-    print(tbl)
-    df = pd.read_html(tbl)
-    print(df)
-    infoDict[title] = df
-    time.sleep(5)
+    outputDict = {}
+    # for each link, we need to query the page, click on availability, get the loan info and save it to a list of dataframes
+    for link in links:
+        driver.get(link)
+        time.sleep(1)
+        # get the title of the book
+        title = driver.find_element_by_xpath('//h5[@class="card-title fullTitleh5"]/span').get_attribute('innerHTML')
+        print('Retrieving info for book: ' + title)
+        output = driver.find_element_by_xpath('//a[@role="button"][@data-target="#holdingsDlg"]')
+        driver.execute_script("arguments[0].click();", output)
+        time.sleep(1)
+        # tbl = driver.find_element_by_xpath('//table[@class="table table-stacked"]')
+        tbl = driver.find_element_by_xpath('//table').get_attribute('outerHTML')
+        # print(tbl)
+        df = pd.read_html(tbl)[0]
+        # print(df)
+        outputDict[title] = df
 
-driver.close()
-
-print(infoDict)
-
-
-
+    driver.close()
+    return(outputDict)
 
 
-# # Library that I want to see
-# selectedLib = 'bishan'
-# if len(sys.argv) > 1:
-#     selectedLib = str(sys.argv[1])
-# # print(str(sys.argv[1]))
-# selectedLibParsed = selectedLib.capitalize()
-# count = 0
+# this function takes in 2 arguments,
+# the first is a dictionary where the key is the book title
+# and the value is a pandas dataframe comprising the complete statuses.
+# the second argument is the library you're searching for
+# it returns a list of triples, with the book title, book status, and number of
+# available copies
 
-# def html_stripper(html):
-#     # remove all special characters used in html tags
-#     x = re.sub('<[^<]+?>', '', str(html))  # gotta coerce to string
-#     return(x)
-
-# # this function, should, given a url, return the book name and status in a tuple
-# # for the selected library
-# def get_book(url):
-
-#     print("Getting book at " + url)
-#     uClient = uReq(url)
-#     page_html = uClient.read()  # stores the content into a variable
-#     uClient.close()
-
-#     page_soup = soup(page_html, "html.parser")
-#     tmp = page_soup.find('table')
-#     # print(tmp)
-
-#     # # have to cast to string before passing into regex
-#     # this regex matches everything except the close bracket, so it can find the title of the book
-#     # the regex on the next line (commented out) will also work, matching all whitespace, characters and colons 
-#     # print(re.search(r"data-title=\"([\w\s\:.]+)\"", str(tmp)))
-#     bookTitle = re.search(r"data-title=\"([^\"]+)\"", str(tmp)).group(1)
-#     print("Book Title is " + bookTitle)
-
-#     statusTable = page_soup.find('table').findAll('tr')
-#     table = html_stripper(statusTable)
-
-#     # we now use regex to find all the library names and availability
-#     libs = re.findall(r"[A-Z][\w\s]* Library|library@[a-z]*", table)
-#     # there are 3 statuses: Available, In-transit, or Due: DD Mmm YYYY 
-#     status = re.findall(r'Available|Due: [0-9]{2} [A-Za-z]{3} [0-9]{4}|In-transit', table)
-#     output = list(zip(libs, status))
-#     # print(output)
-
-#     # test = [v for i, v in enumerate(output) if selectedLib in i]
-#     def findStatus(lst):
-#         global count 
-#         global selectedLibParsed
-#         count = 0 
-#         tmp = ''
-#         for item in lst:
-#             if selectedLib.lower() in item[0].lower():
-#                 selectedLibParsed = item[0]
-#                 # print(item[0], item[1])
-#                 if (item[1] == "Available"):
-#                     count += 1
-#                 else:
-#     # # TODO: if multiple on loan, return earlier loan date. transit should also take prio over loan
-#                     tmp = item[1]
+def filterBookInfoByLib(dict, lib):
+    outputList = []
+    for key in dict:
+        df = dict.get(key)
+        filteredDf = df[df['Library'].str.contains(lib.capitalize())][["Item Status"]].values.tolist()
+        # flatten the list of lists and add it to the dict
+        flatList = [item for sublist in filteredDf for item in sublist]
+        sorted(flatList)
+        if len(flatList) == 0:
+            triple = (key, "not available at this library", 0)
+            outputList.append(triple)
+        else:
+            count = flatList.count("Available")
+            if count == 0:
+                triple = (key, flatList[0], 0)
+                outputList.append(triple)
+            else:
+                triple = (key, "Available", count)
+                outputList.append(triple)
+    return outputList
 
 
-#         if count == 0:
-#             if tmp == '':
-#                 return("not available at this library")
-#             return (tmp)
-#         # print("There is " + str(count) + " copy available for loan at " + selectedLibParsed) if count == 1 else print("There are " + str(count) + " copies available for loan at " + selectedLib) 
-#         return ("Available")
+# this function takes in the list of triples and prints nice stuff for the user to see
 
-#     bookStatus = findStatus(output)
-#     # print(bookTitle + ' is ' + bookStatus + ' at ' + selectedLibParsed)
-    
-
-#     # print(bookTitle, bookStatus, count)
-#     return (bookTitle, bookStatus, count)
-
-#     # testing print statements
-#     # print(page_soup)
-#     # print(tmp)
-#     # print(table)
-#     # return
+def userOutputSingleLib(tripList, lib):
+    availFlag = False # this flag tracks if there are any books available
+    printListAvail = []
+    printListUnavail = []
+    lib = lib.capitalize()
+    for i in tripList:
+        if i[1] == "Available":
+            availFlag = True
+            tmpPrint = (str(i[2]) + " copy available for loan.") if i[2] == 1 else (str(i[2]) + " copies available for loan.")
+            printListAvail.append(i[0] + ": " + tmpPrint)
+        else:
+            tmpPrint = (i[0] + " is " + i[1])
+            printListUnavail.append(tmpPrint)
 
 
+    print("Getting Books:\n")
+    if availFlag:
+        tmp = ("The Available books at " + lib + " are:\n")
+        print(colored(tmp, 'red'))
+        for i in printListAvail:
+            print(i)
+        print("\n")
+    else:
+        print("None of the books are available at " + lib + " \U0001f97A\n" )
+    tmp = ("Here is the status of the Unavailable Books:\n")
+    print(colored(tmp, 'red'))
+    for i in printListUnavail:
+        print(i)
+    return
 
-# # iterate over the books with the above function 
-# summaryList = []
-# for i in books:
-#     summaryList.append(get_book(i))
+################################################################################################################################
+############################################# DO NOT EDIT THE PART ABOVE #######################################################
 
-# # print(summaryList)
+# Only edit the selected library, as well as your user info in the other document
 
-# # OUTPUT TO USER:
-# availFlag = False # this flag tracks if there are any books available
-
-# printListAvail = []
-# printListUnavail = []
-# for i in summaryList:
-#     if i[1] == "Available":
-#         availFlag = True
-#         tmpPrint = (str(i[2]) + " copy available for loan.") if i[2] == 1 else (str(i[2]) + " copies available for loan.") 
-#         printListAvail.append(i[0] + ": " + tmpPrint)
-#     else: 
-#         tmpPrint = (i[0] + " is " + i[1])
-#         printListUnavail.append(tmpPrint)
-
-# print("Getting Books:\n")
-# if availFlag:
-#     tmp = ("The Available books at " + selectedLibParsed + " are:\n")
-#     print(colored(tmp, 'red'))
-#     for i in printListAvail: 
-#         print(i)
-#     print("\n")
-# else:
-#     print("None of the books are available at " + selectedLibParsed + " \U0001f97A\n" )
-# tmp = ("Here is the status of the Unavailable Books:\n")
-# print(colored(tmp, 'red'))
-# for i in printListUnavail: 
-#     print(i)
-
-# # # useful regex 
-# # # get lib names
-# # re.findall(r'[A-Z][\w\s]* Library', table)
-# # # get all statuses 
-# # re.findall(r'(Available|Due: [0-9]{2} [A-Za-z]{3} [0-9]{4}),', table)
-
-# # test cases 
-# # get_book('https://catalogue.nlb.gov.sg/cgi-bin/spydus.exe/XHLD/WPAC/BIBENQ/95357198/246739648') #works MILKMAN
-# # get_book('https://catalogue.nlb.gov.sg/cgi-bin/spydus.exe/XHLD/WPAC/BIBENQ/95360467/171894760') #works Stories of your life 
-# # get_book('https://catalogue.nlb.gov.sg/cgi-bin/spydus.exe/XHLD/WPAC/BIBENQ/95365926/178142215') #works Do Not Say We Have Nothing
-# # get_book('https://catalogue.nlb.gov.sg/cgi-bin/spydus.exe/XHLD/WPAC/BIBENQ/95370061/312945068') #works Galatea
+selectedLib = 'bishan' # EDIT THIS
+rawInfo = scrapeAllBookInfo()
+filteredInfoSingleLib = filterBookInfoByLib(rawInfo, selectedLib)
+userOutputSingleLib(filteredInfoSingleLib, selectedLib)
